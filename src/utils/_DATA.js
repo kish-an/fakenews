121b88f8def6098c777c9874e4d1d8b6dc949188
Kishan <kishan.o@hotmail.com>
2020-03-11 20:44:52 +0000

    Fix typo

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -12,8 +12,8 @@ let users = {
     tweets: ['5c9qojr2d1738zlx09afby', 'f4xzgapq7mu783k9t02ghx', 'nnvkjqoevs8t02lzcc0ky', '4pt0px8l0l9g6y69ylivti', 'fap8sdxppna8oabnxljzcv', 'leqp4lzfox7cqvsgdj0e7', '26p5pskqi88i58qmza2gid', 'xi3ca2jcfvpa0i3t4m7ag'],
   },
   McConaughey: {
-    id: "Matthew McConaughey",
-    name: "Dan Abramov",
+    id: "McConaughey",
+    name: "Matthew McConaughey",
     avatarURL: "https://pbs.twimg.com/profile_images/1191381171164237824/jdS95Rtm_400x400.jpg",
     tweets: ['5w6k1n34dkp1x29cuzn2zn', 'czpa59mg577x1oo45cup0d', 'omdbjl68fxact38hk7ypy6', '3km0v4hf1ps92ajf4z2ytg', 'njv20mq7jsxa6bgsqc97', 'sfljgka8pfddbcer8nuxv', 'r0xu2v1qrxa6ygtvf2rkjw'],
   }
